86b5830f6219ab7072769f1694eb953133ee8df0
NullBy1e <68380685+NullBy1e@users.noreply.github.com>
2021-05-15 12:46:32 +0200

    Changed indentations

--- config.py
+++ config.py
@@ -3,29 +3,29 @@ import json
 
 
 def read_config():
-    # * Read config and return json
-    with open("config.json", "r") as file:
-        json_data = json.load(file)
-        return json_data
-    log("INFO", "Reading from config")
+	# * Read config and return json
+	with open("config.json", "r") as file:
+		json_data = json.load(file)
+		return json_data
+	log("INFO", "Reading from config")
 
 
 def parse_config(conf_json, variable):
-    if variable == "Token":
-        return conf_json["Token"]
-    else:
-        raise Exception("Can't find specified id in config")
+	if variable == "Token":
+		return conf_json["Token"]
+	else:
+		raise Exception("Can't find specified id in config")
 
 
 def get_variable_from_config(name):
-    variable = parse_config(read_config(), name)
-    return variable
+	variable = parse_config(read_config(), name)
+	return variable
 
 
 def write_variable_to_config(name, value):
-    variable = {name: value}
-    dic = read_config()
-    dic.update(variable)
-    with open("config.json", "w") as f:
-        json.dump(dic, f)
-    log("INFO", "Writing to Config: {}".format(str(name) + ";" + str(value)))
+	variable = {name: value}
+	dic = read_config()
+	dic.update(variable)
+	with open("config.json", "w") as f:
+		json.dump(dic, f)
+	log("INFO", "Writing to Config: {}".format(str(name) + ";" + str(value)))
