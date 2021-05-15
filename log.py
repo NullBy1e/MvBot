44bc2b7d4f46f8b190651d5923b8bb48650e39de
NullBy1e <68380685+NullBy1e@users.noreply.github.com>
2021-05-15 12:45:51 +0200

    Changed indentation

--- log.py
+++ log.py
@@ -4,30 +4,29 @@ import datetime
 
 
 def getDate():
-    date = datetime.datetime.now().strftime("%d%m%Y")
-    return date
+	date = datetime.datetime.now().strftime("%d%m%Y")
+	return date
 
 
 def getTime():
-    offset = datetime.timezone(datetime.timedelta(hours=1))
-    Hours = datetime.datetime.now(offset).strftime("%H")
-    Minutes = datetime.datetime.now().strftime("%M")
-    Seconds = datetime.datetime.now().strftime("%S")
-    Time = Hours + ":" + Minutes + ":" + Seconds
-    return Time
+	offset = datetime.timezone(datetime.timedelta(hours=1))
+	Hours = datetime.datetime.now(offset).strftime("%H")
+	Minutes = datetime.datetime.now().strftime("%M")
+	Seconds = datetime.datetime.now().strftime("%S")
+	Time = Hours + ":" + Minutes + ":" + Seconds
+	return Time
 
 
 def log(Type, Message):
-    date = getDate()
-    time = getTime()
-    try:
-        logFile = open("Logs/{}.log".format(str(date)), "a")
-    except:
-        os.touch("Logs/{}.log".format(str(date)))
-        logFile = open("Logs/{}.log".format(str(date)), "a")
-    logFile.write(time + " " + Type + " " + Message + "\n")
-
+	date = getDate()
+	time = getTime()
+	try:
+		logFile = open("Logs/{}.log".format(str(date)), "a")
+	except:
+		os.touch("Logs/{}.log".format(str(date)))
+		logFile = open("Logs/{}.log".format(str(date)), "a")
+	logFile.write(time + " " + Type + " " + Message + "\n")
 
 def check_logs():
-    #TODO: Check if the logs contain errors
-    pass
+	#TODO: Check if the logs contain errors
+	pass
