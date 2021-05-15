bf5db1bc7ea32db15104c401abbbc9b68fecce1c
NullBy1e <68380685+NullBy1e@users.noreply.github.com>
2021-05-15 12:18:32 +0200

    Added logging to the script

---
+++ log.py
@@ -0,0 +1,28 @@
+import time
+import os
+import datetime
+
+
+def getDate():
+    date = datetime.datetime.now().strftime("%d%m%Y")
+    return date
+
+
+def getTime():
+    offset = datetime.timezone(datetime.timedelta(hours=1))
+    Hours = datetime.datetime.now(offset).strftime("%H")
+    Minutes = datetime.datetime.now().strftime("%M")
+    Seconds = datetime.datetime.now().strftime("%S")
+    Time = Hours + ":" + Minutes + ":" + Seconds
+    return Time
+
+
+def log(Type, Message):
+    date = getDate()
+    time = getTime()
+    try:
+        logFile = open("Logs/{}.log".format(str(date)), "a")
+    except:
+        os.touch("Logs/{}.log".format(str(date)))
+        logFile = open("Logs/{}.log".format(str(date)), "a")
+    logFile.write(time + " " + Type + " " + Message + "\n")
